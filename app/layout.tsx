8531083eb3a680efce7bb321aa88b942a5d53544
Ivan Bondar <ivanbondar133@gmail.com>
2025-12-04 14:29:44 +0200

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+      <title>Ivan Bondar - Full-Stack Developer</title>
+      <link rel="icon" href="/favicon.png" sizes="any" />
+    </head>
       <body>{children}</body>
     </html>
   );
